d3c2078bad461ae368ad95fcfe83ed237f0306df
ecenteno7 <erikcenteno@proton.me>
2024-06-22 19:49:43 -0400

    Added tailwind to project, started skeleton layout for site

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,22 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, welcome to Chili's.
-        </p>
-      </header>
+        <div className="bg-slate-800 text-white font-bold text-3xl justify-items-start">
+          <div className="max-w-sm text-left">
+            <p className="pl-10 pt-3 pb-3">Blink</p>
+          </div>
+        </div>
+        <div className="bg-slate-300 max-w-2xl min-h-screen grid grid-rows-12">
+          <div className="p-3 mg-slate-500 w-full row-span-2">
+            <p>Insights</p>
+          </div> 
+          <div className="row-span-8">
+            <p>Body</p>
+          </div>
+          <div className="row-span-2">
+            <p>Chatter</p>
+          </div>
+        </div>
     </div >
   );
 }
