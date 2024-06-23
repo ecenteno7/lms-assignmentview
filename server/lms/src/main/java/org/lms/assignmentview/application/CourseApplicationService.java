f911749648a6ee1950d38936358c235c80c98f5c
hwittenb <75zQE46CTqtA>
2024-06-22 23:30:09 -0400

    add course related apis

--- server/lms/src/main/java/org/lms/assignmentview/application/CourseApplicationService.java
+++ server/lms/src/main/java/org/lms/assignmentview/application/CourseApplicationService.java
@@ -1,12 +1,15 @@
 package org.lms.assignmentview.application;
 
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.lms.assignmentview.domain.course.Course;
+import org.lms.assignmentview.domain.course.CourseId;
 import org.lms.assignmentview.domain.course.CourseService;
 import org.lms.assignmentview.domain.course.commands.CreateCourseCommand;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +21,16 @@ public class CourseApplicationService {
     @Transactional
     public @NonNull Course createCourse(@NonNull final CreateCourseCommand createCourseCommand) {
         return courseService.createCourse(createCourseCommand);
+    }
+
+    @Transactional(readOnly = true)
+    public @NonNull List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @Transactional(readOnly = true)
+    public @NonNull Course getCourseById(@NonNull final CourseId courseId) {
+        return courseService.getCourseById(courseId);
     }
 
 }
--- server/lms/src/main/java/org/lms/assignmentview/presentation/rest/CourseController.java
+++ server/lms/src/main/java/org/lms/assignmentview/presentation/rest/CourseController.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.lms.assignmentview.application.CourseApplicationService;
 import org.lms.assignmentview.domain.course.Course;
+import org.lms.assignmentview.domain.course.CourseId;
 import org.lms.assignmentview.domain.course.commands.CreateCourseCommand;
 import org.lms.assignmentview.presentation.rest.dto.course.CourseRequestDto;
 import org.lms.assignmentview.presentation.rest.dto.course.CourseResponseDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,13 +19,25 @@ public class CourseController {
     @NonNull
     private final CourseApplicationService courseApplicationService;
 
-    @PostMapping("/api/course")
+    @PostMapping("/api/admin/courses")
     public @NonNull CourseResponseDto createCourse(@RequestBody final CourseRequestDto courseRequestDto) {
         if (courseRequestDto.courses().size() != 1) {
             throw new IllegalStateException("Application only supports adding a single course at a time.");
         }
         final CreateCourseCommand createCourseCommand = courseRequestDto.courses().get(0).toCreateCourseCommand();
         final Course course = courseApplicationService.createCourse(createCourseCommand);
+        return CourseResponseDto.from(List.of(course));
+    }
+
+    @GetMapping("/api/admin/courses")
+    public @NonNull CourseResponseDto getAllCourses() {
+        final List<Course> courses = courseApplicationService.getAllCourses();
+        return CourseResponseDto.from(courses);
+    }
+
+    @GetMapping("/api/admin/courses/{course-id}")
+    public @NonNull CourseResponseDto getCourseById(@PathVariable("course-id") @NonNull final String courseId) {
+        final Course course = courseApplicationService.getCourseById(new CourseId(courseId));
         return CourseResponseDto.from(List.of(course));
     }
 
