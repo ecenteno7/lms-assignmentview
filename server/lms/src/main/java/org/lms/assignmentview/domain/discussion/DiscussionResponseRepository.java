43454f9a98723c5fd5a30b57544df922d288c71d
Harrison Wittenbrook <harrisonwittenbrook@gmail.com>
2024-07-06 11:16:59 -0400

    add ability to accept a response

--- server/lms/src/main/java/org/lms/assignmentview/domain/discussion/DiscussionResponseRepository.java
+++ server/lms/src/main/java/org/lms/assignmentview/domain/discussion/DiscussionResponseRepository.java
@@ -3,10 +3,17 @@ package org.lms.assignmentview.domain.discussion;
 import lombok.NonNull;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiscussionResponseRepository {
 
     @NonNull
-    List<DiscussionResponse> saveAllResponses(@NonNull List<DiscussionResponse> discussionResponses);
+    DiscussionResponse save(@NonNull final DiscussionResponse discussionResponse);
+
+    @NonNull
+    List<DiscussionResponse> saveAllResponses(@NonNull final List<DiscussionResponse> discussionResponses);
+
+    @NonNull
+    Optional<DiscussionResponse> getDiscussionResponseById(@NonNull final DiscussionResponseId discussionResponseId);
 
 }
