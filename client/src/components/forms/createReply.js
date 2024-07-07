7497ce755846de1f4be6cc3a8de48b880aae0daa
ecenteno7 <erikcenteno@proton.me>
2024-07-07 13:35:40 -0400

    Clear Text Box on Submit
    - Cleared text box on reply success submit

--- client/src/components/forms/createReply.js
+++ client/src/components/forms/createReply.js
@@ -10,13 +10,14 @@ export const CreateReply = ({authorId, courseId, discussionPostId, refresh}) => 
       .then(res => {
         if (res.status == 200){
           refresh()
+          setReply("")
         }
       })   
   }
 
   return (
     <div className="pb-4 pt-4 h-60 flex flex-row">
-      <textarea onChange={e=>setReply(e.target.value)} type="text" id="search" class="bg-slate-200 text-gray-900 text-sm outline-none rounded-xl w-full h-full p-2.5 " placeholder="Type your response here..." />
+      <textarea onChange={e=>setReply(e.target.value)} value={reply} type="text" id="search" class="bg-slate-200 text-gray-900 text-sm outline-none rounded-xl w-full h-full p-2.5 " placeholder="Type your response here..." />
       <button onClick={handleSubmit} className="ml-2 bg-slate-800 rounded-xl text-white font-bold p-2">Submit</button> 
       </div>
   )
